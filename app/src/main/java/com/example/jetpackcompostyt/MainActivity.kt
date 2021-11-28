ab44292e9d2958e96c4683e514a3a817aa5aca89
foxandroid <foxstream001@gmail.com>
2021-11-28 22:59:39 +0530

    Modifiers

--- app/src/main/java/com/example/jetpackcompostyt/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompostyt/MainActivity.kt
@@ -3,30 +3,35 @@ package com.example.jetpackcompostyt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcompostyt.ui.theme.JetpackCompostYTTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting("FoxAndroid - Like, share, subscribe!")
+            /*Greeting("FoxAndroid - Like, share, subscribe!")*/
         }
     }
 }
 
-@Composable
+/*@Composable
 fun Greeting(name: String) {
     Column(
         modifier = Modifier
@@ -39,6 +44,29 @@ fun Greeting(name: String) {
         Text(text = "Share")
         Text(text = "Subscribe")
     }
+}*/
+
+@Composable
+fun Card(person: Person){
+
+    Row(verticalAlignment = Alignment.CenterVertically,
+    modifier = Modifier.padding(8.dp)){
+        
+        Image(painter = painterResource(id = R.drawable.robert),
+            contentDescription = "Person Profile",
+            contentScale = ContentScale.Crop,
+        modifier = Modifier.size(80.dp)
+            .clip(CircleShape))
+        
+        Column(modifier = Modifier.padding(8.dp)){
+            Text(text = person.name, fontSize = 40.sp)
+            Text(text = person.lastSeen,
+            color = Color.Gray,
+            fontSize = 20.sp)
+        }
+
+    }
+
 
 
 }
@@ -47,6 +75,8 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetpackCompostYTTheme {
-        Greeting("FoxAndroid")
+        /*Greeting("FoxAndroid")*/
+        val person = Person("Robert Downey", "3 minutes ago")
+        Card(person)
     }
 }
---
+++ app/src/main/java/com/example/jetpackcompostyt/Person.kt
@@ -0,0 +1,3 @@
+package com.example.jetpackcompostyt
+
+data class Person(val name : String, val lastSeen : String)
